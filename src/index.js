d831232a97c52b61ac48a16d3eba67a1229d2017
Ben Daniels <benbanj@hotmail.com>
2021-12-27 13:28:05 +0000

    Adding Homepage functionality
    - Adding a class to the home nav element
    - Adding event listener to home nav element in index.js
    - Adding showHomePage function to manage loading homepage
    - Adding clearContent function to clear current content before tab
    switching

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import loadContactPage from "./contact.js";
 
 pageLoadModule.run();
 
+const content = document.querySelector('#content');
 const contact = document.querySelector('.contact');
+const home = document.querySelector('.home');
 contact.addEventListener('click', showContactPage);
+home.addEventListener('click', showHomePage);
+
+function clearContent() {
+  content.innerHTML = null;
+}
+
+function showHomePage() {
+  clearContent();
+  pageLoadModule.run();
+}
 
 function showContactPage() {
+  clearContent();
   loadContactPage.run();
 }
